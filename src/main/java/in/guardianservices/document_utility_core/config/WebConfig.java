b14db2d2a1fc822ffbdac7fdbadce7ba3efb1cc7
rahulsharma <rsharma0374@gmail.com>
2025-07-08 01:38:46 +0530

    commented cors

--- src/main/java/in/guardianservices/document_utility_core/config/WebConfig.java
+++ src/main/java/in/guardianservices/document_utility_core/config/WebConfig.java
@@ -1,19 +1,19 @@
-package in.guardianservices.document_utility_core.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173",
-                        "https://docutil.guardianservices.in") // frontend origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//package in.guardianservices.document_utility_core.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173",
+//                        "https://docutil.guardianservices.in") // frontend origin
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
