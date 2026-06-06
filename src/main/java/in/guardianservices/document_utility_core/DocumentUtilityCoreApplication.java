4c1b30a70ad43d237106df78e324c7de59b43f8a
Rahul Sharma <rahulsha@lentra.ai>
2026-06-06 22:40:57 +0530

    updated doc service

--- src/main/java/in/guardianservices/document_utility_core/DocumentUtilityCoreApplication.java
+++ src/main/java/in/guardianservices/document_utility_core/DocumentUtilityCoreApplication.java
@@ -2,8 +2,10 @@ package in.guardianservices.document_utility_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DocumentUtilityCoreApplication {
 
 	public static void main(String[] args) {
