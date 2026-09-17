51f44eee294755a8b0d82640341a557512a29536
Rahul Sharma <rahulsha@lentra.ai>
2026-09-17 17:29:33 +0530

    updated dependency

--- src/main/java/in/guardianservices/document_utility_core/DocumentUtilityCoreApplication.java
+++ src/main/java/in/guardianservices/document_utility_core/DocumentUtilityCoreApplication.java
@@ -2,10 +2,8 @@ package in.guardianservices.document_utility_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class DocumentUtilityCoreApplication {
 
 	public static void main(String[] args) {
